b3275ce0f3481178bedd5a1d705a8e993c0b34cd
Moritz Müller <moritz-2@gmx.net>
2026-03-16 10:52:14 +0100

    Implemented Embedding Loader

--- backend/app/services/embeddings.py
+++ backend/app/services/embeddings.py
@@ -1,5 +1,18 @@
-from typing import List
+from typing import List, Text
+import logging
 
+
+logging.basicConfig(level = logging.INFO)
+logger = logging.getLogger(__name__)
+
+class Embedding:
+  
+  MODEL_NAME = "Qwen/Qwen3-Embedding-0.6B"
+    
+  def __init__(self):
+    from sentence_transformers import SentenceTransformer
+    self.embedding = SentenceTransformer(self.MODEL_NAME)
+    logger.info("Embedding Model Loaded")
 
 def generate_embedding(text: str) -> List[float]:
     raise NotImplementedError("Embedding service not implemented yet")
